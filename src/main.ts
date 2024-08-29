6ad1de4b166636b86223843a72f881de3e2a68a8
Sistemas <sistemas@esmeraschool.com>
2024-08-29 15:17:33 +0200

    add morgan for logs

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import rawBodyMiddleware from './middlewares/raw-body.middleware';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(morgan('dev')); // Puedes cambiar 'dev' por otros formatos como 'combined', 'common', etc.
   app.use(rawBodyMiddleware());
 
   // Habilitar validación global
   app.useGlobalPipes(new ValidationPipe());
 
+  // Haiblitar Morgan
   // Habilitar CORS
   app.enableCors({
     origin: 'http://localhost:5173', // Reemplaza con la URL de tu frontend
